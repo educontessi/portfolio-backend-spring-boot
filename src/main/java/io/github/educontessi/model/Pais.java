0b3a842f3e3f84c2159ea683cb671a71f65b117b
Eduardo Contessi <contessi@outlook.com>
2019-06-17 00:38:42 -0300

    first commit

---
+++ src/main/java/io/github/educontessi/model/Pais.java
@@ -0,0 +1,39 @@
+package io.github.educontessi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "pais")
+public class Pais {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	@Size(min = 3, max = 100)
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
