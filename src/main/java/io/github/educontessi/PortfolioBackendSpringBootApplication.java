95491660a5be15e1c2fe0f4d86f0c94e845c18e8
Eduardo Contessi <Eduardo.Contessi@thomsonreuters.com>
2019-06-28 14:15:04 -0300

    ajuste pom.xml

--- src/main/java/io/github/educontessi/PortfolioBackendSpringBootApplication.java
+++ src/main/java/io/github/educontessi/PortfolioBackendSpringBootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PortfolioBackendSpringBootApplication {
 
 	public static void main(String[] args) {
+		// mvnw spring-boot:run
 		SpringApplication.run(PortfolioBackendSpringBootApplication.class, args);
 	}
 
