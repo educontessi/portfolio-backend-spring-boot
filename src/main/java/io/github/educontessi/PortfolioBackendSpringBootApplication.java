d0d11b6cd90f9bfb9868bfba9d06a0336d5496f5
Eduardo Contessi <contessi@outlook.com>
2019-07-03 21:58:44 -0300

    ajuste fuso horario mysql

--- src/main/java/io/github/educontessi/PortfolioBackendSpringBootApplication.java
+++ src/main/java/io/github/educontessi/PortfolioBackendSpringBootApplication.java
@@ -1,10 +1,19 @@
 package io.github.educontessi;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PortfolioBackendSpringBootApplication {
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("TimeZone"));
+	}
 
 	public static void main(String[] args) {
 		// mvnw spring-boot:run
