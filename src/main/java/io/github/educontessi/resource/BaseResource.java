4e599617e7d69990dd3950b5b357317e3b773dce
Eduardo Contessi <Eduardo.Contessi@thomsonreuters.com>
2019-12-12 15:56:35 -0300

    baseResource

--- src/main/java/io/github/educontessi/resource/BaseResource.java
+++ src/main/java/io/github/educontessi/resource/BaseResource.java
@@ -1,5 +1,21 @@
 package io.github.educontessi.resource;
 
-public abstract class BaseResource {
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import io.github.educontessi.helpers.event.RecursoCriadoEvent;
+
+public abstract class BaseResource<E> {
+
+	@Autowired
+	private ApplicationEventPublisher publisher;
+
+	protected ResponseEntity<E> created(Long id, HttpServletResponse response, E body) {
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, id));
+		return ResponseEntity.status(HttpStatus.CREATED).body(body);
+	}
 }
