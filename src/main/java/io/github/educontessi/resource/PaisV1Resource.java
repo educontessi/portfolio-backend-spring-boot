648e793c4236a076820010178ef0e5b2f5bae316
Eduardo Contessi <Eduardo.Contessi@thomsonreuters.com>
2019-12-27 16:38:11 -0300

    ajuste documentacao da api com swagger

--- src/main/java/io/github/educontessi/resource/PaisV1Resource.java
+++ src/main/java/io/github/educontessi/resource/PaisV1Resource.java
@@ -25,6 +25,11 @@ import io.github.educontessi.dataconverter.PaisV1DataConverter;
 import io.github.educontessi.dto.PaisV1Dto;
 import io.github.educontessi.model.Pais;
 import io.github.educontessi.service.PaisService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 /**
  * Endpoints para {@link Pais}
@@ -34,6 +39,7 @@ import io.github.educontessi.service.PaisService;
  */
 @RestController
 @RequestMapping("/v1/paises")
+@Api(produces = "application/json", value = "PaisV1Resource")
 public class PaisV1Resource extends BaseResource {
 
 	@Autowired
@@ -43,6 +49,12 @@ public class PaisV1Resource extends BaseResource {
 	private PaisV1DataConverter converter;
 
 	@GetMapping
+	@ApiOperation(value = "Busca todos", response = Iterable.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Recuperou com sucesso todos os recursos"),
+			@ApiResponse(code = 401, message = "Você não está autorizado a visualizar o recurso"),
+			@ApiResponse(code = 403, message = "É proibido acessar o recurso que você está tentando acessar"),
+			@ApiResponse(code = 404, message = "O recurso que você estava tentando acessar não foi encontrado"),
+			@ApiResponse(code = 500, message = "O aplicativo servidor falhou ao processar a solicitação") })
 	public List<PaisV1Dto> findAll() {
 		List<Pais> lista = service.findAll();
 		List<PaisV1Dto> listaDto = new ArrayList<>();
@@ -52,14 +64,28 @@ public class PaisV1Resource extends BaseResource {
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<PaisV1Dto> findById(@PathVariable Long id) {
+	@ApiOperation(value = "Busca por ID", response = Iterable.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Recurso recuperado com sucesso"),
+			@ApiResponse(code = 401, message = "Você não está autorizado a visualizar o recurso"),
+			@ApiResponse(code = 403, message = "É proibido acessar o recurso que você está tentando acessar"),
+			@ApiResponse(code = 404, message = "O recurso que você estava tentando acessar não foi encontrado"),
+			@ApiResponse(code = 500, message = "O aplicativo servidor falhou ao processar a solicitação") })
+	public ResponseEntity<PaisV1Dto> findById(
+			@ApiParam(value = "Código do país", required = true) @PathVariable Long id) {
 		Optional<Pais> categoria = service.findById(id);
 		return categoria.isPresent() ? ResponseEntity.ok(converter.convertToDto(categoria.get()))
 				: ResponseEntity.notFound().build();
 	}
 
 	@PostMapping
-	public ResponseEntity<Object> save(@Valid @RequestBody PaisV1Dto dto, HttpServletResponse response) {
+	@ApiOperation(value = "Salvar", response = Iterable.class)
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Recurso salvo com sucesso"),
+			@ApiResponse(code = 401, message = "Você não está autorizado a salvar o recurso"),
+			@ApiResponse(code = 403, message = "É proibido acessar o recurso que você está tentando acessar"),
+			@ApiResponse(code = 500, message = "O aplicativo servidor falhou ao processar a solicitação") })
+	public ResponseEntity<Object> save(
+			@ApiParam(value = "Objeto DTO país para salvar", required = true) @Valid @RequestBody PaisV1Dto dto,
+			HttpServletResponse response) {
 		Pais entity = new Pais();
 		converter.copyToEntity(entity, dto);
 		entity = service.save(entity);
@@ -69,7 +95,14 @@ public class PaisV1Resource extends BaseResource {
 	}
 
 	@PutMapping("/{id}")
-	public ResponseEntity<Object> update(@PathVariable Long id, @Valid @RequestBody Pais entity) {
+	@ApiOperation(value = "Alterar", response = Iterable.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Recurso alterado com sucesso"),
+			@ApiResponse(code = 401, message = "Você não está autorizado a alterar o recurso"),
+			@ApiResponse(code = 403, message = "É proibido acessar o recurso que você está tentando acessar"),
+			@ApiResponse(code = 404, message = "O recurso que você estava tentando acessar não foi encontrado"),
+			@ApiResponse(code = 500, message = "O aplicativo servidor falhou ao processar a solicitação") })
+	public ResponseEntity<Object> update(@ApiParam(value = "Código do país", required = true) @PathVariable Long id,
+			@ApiParam(value = "Objeto DTO país para alterar", required = true) @Valid @RequestBody Pais entity) {
 		try {
 			Pais pais = service.update(id, entity);
 			return ok(pais);
@@ -79,6 +112,12 @@ public class PaisV1Resource extends BaseResource {
 	}
 
 	@DeleteMapping("/{id}")
+	@ApiOperation(value = "Alterar", response = Iterable.class)
+	@ApiResponses(value = { @ApiResponse(code = 204, message = "Recurso excluído com sucesso"),
+			@ApiResponse(code = 401, message = "Você não está autorizado a excluir o recurso"),
+			@ApiResponse(code = 403, message = "É proibido acessar o recurso que você está tentando acessar"),
+			@ApiResponse(code = 404, message = "O recurso que você estava tentando excluir não foi encontrado"),
+			@ApiResponse(code = 500, message = "O aplicativo servidor falhou ao processar a solicitação") })
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long id) {
 		service.delete(id);
